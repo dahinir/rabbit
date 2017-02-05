49c5c2e33b730d120e798b00ca5b6a4ebade8fbd
Daehyeon Shin <dahini@gmail.com>
2017-02-05 23:46:45 +0900

    let go

--- addMachine.js
+++ addMachine.js
@@ -4,12 +4,12 @@ const Machine = require('./machine.js').Machine,
 // const Machines = require('./machine.js').Machines;
 
 // add new machines by newMachines.json
-// var newMachines = new Machines(require('./addMachine.json'));
+// let newMachines = new Machines(require('./addMachine.json'));
 // newMachines.each(function(m){
 // 	m.save();
 // });
 
-var setting = {
+let setting = {
     "propensity": "hot",
     "craving_krw": 2000,
     "cravingRatio": 0.5,
@@ -20,23 +20,29 @@ var setting = {
     "maxHope": 10000,
     "status": "krw"
 };
-var settings = [];
+let settings = [];
 // setting.propensity = "cold";
-var machine_count = 0;
+let machine_count = 0;
+let MIN_HOPE = -50000,
+    MAX_HOPE = 50000;
 
-// positiveHope - negativeHope is gap
-for (let gap = 2000; gap <= 40000; gap += 100) {
-    // console.log("===");
-    setting.craving_krw = gap; // it's easy way..
+for (let cravingRatio = 0.5; cravingRatio <= 0.9; cravingRatio += 0.1) {
+    setting.cravingRatio = cravingRatio.toFixed(1)*1;
 
-    for (let step = 0; step <= 20000; step += 500) {
-        setting.negativeHope = setting.neverHope = -20000 + step;
-        setting.positiveHope = setting.maxHope = setting.negativeHope + gap;
-        settings.push(_.extend({}, setting));
-        machine_count++;
-        // console.log(setting.negativeHope, setting.positiveHope, gap, setting.craving_krw);
-        if (setting.positiveHope >= 20000)
-            break;
+    // positiveHope - negativeHope is gap
+    for (let gap = 2000; gap <= 40000; gap += 1000) {
+        // console.log("\n");
+        setting.craving_krw = gap; // it's just easy way..
+
+        for (let step = 0; ; step += 1000) {
+            setting.negativeHope = setting.neverHope = MIN_HOPE + step;
+            setting.positiveHope = setting.maxHope = setting.negativeHope + gap;
+            settings.push(_.extend({}, setting));
+            machine_count++;
+            // console.log(setting.negativeHope, setting.positiveHope, "", setting.craving_krw, setting.cravingRatio);
+            if (setting.positiveHope >= MAX_HOPE)
+                break;
+        }
     }
 }
 
@@ -44,11 +50,12 @@ function save(i) {
     if (settings[i])
         new Machine().save(settings[i], {
             success: function() {
-                (i%1000==0)?console.log(i):0;
+                (i % 1000 == 0) ? console.log(i): 0;
                 save(i + 1);
             }
         });
 }
-// save(0);
+save(0);
 
 console.log("\nadded machines:", machine_count, settings.length);
+console.log("Rabbit needs", machine_count * 1000, "won");
